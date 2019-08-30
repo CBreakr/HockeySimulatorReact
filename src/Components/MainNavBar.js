25d8975a06a85a26bfb5e9858cdbe5310d68853a
CBreakr <rob.notwicz@gmail.com>
2019-08-30 12:58:22 -0400

    added home and game to navbar

--- src/Components/MainNavBar.js
+++ src/Components/MainNavBar.js
@@ -2,13 +2,17 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const style = {
+  color:"green",
+  fontWeight:"bold"
+};
+
 class MainNavBar extends React.Component {
   render(){
     return (
       <div>
-        <h2>
-          Nav Bar
-        </h2>
+        <NavLink exact to="/" activeStyle={style}>Home</NavLink>
+        <NavLink exact to="/game" activeStyle={style}>Game</NavLink>
       </div>
     );
   }
